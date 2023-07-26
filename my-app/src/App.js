18cac9613a87be8683471b146c80c0a234e3360d
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-07-26 18:42:27 +0530

    updated dark mode to next level

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,32 @@
 // import logo from './logo.svg';
 import './App.css';
-// import TextForm from './components/TextForm';
-import About from './components/About';
+import TextForm from './components/TextForm';
+// import About from './components/About';
 import Navbar from './components/Navbar';
+import React,{useState} from 'react'
+
+
 let aboutText="About";
 let name="Text Modifier Website";
 function App() {
+  const[mode,setMode]=useState("dark");
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
+
   return (
-    <>
-    
-  <Navbar title={name} aboutText={aboutText} />
+    <> 
+  <Navbar title={name} aboutText={aboutText} mode={mode} toggleMode={toggleMode} />
   <div className="container my-5">
-  {/* <TextForm heading="Enter the text to Analyse"/> */}
-   <About/>
+  <TextForm heading="Enter the text to Analyse" mode={mode}/>
+   {/* <About/> */}
   </div>
   
     {/* <Navbar title='Capitalizer'/>
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -65,21 +65,21 @@ export default function TextForm(props) {
         }
   return (
      <>
-     <div className="container">
-        
+     <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+        <h1>{props.heading}</h1>
         <div className="mb-3">
-        <label htmlFor="myBox" className="form-label">{props.heading}</label>
-        <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
+        
+        <textarea style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} className="form-control" id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
       </div>
       <button className="btn btn-primary " onClick={handleUpClick} >Convert To Uppercase</button>
       <button className="btn btn-primary mx-2" onClick={handleUpClick2} >Convert To Lowercase</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008*(text.split(" ").length)} minutes it will take to read this..</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter text to Preview"}</p>
     </div>
 
      </>
