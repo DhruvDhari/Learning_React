934236c089bd584cc083ab1acfed0f4733705318
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-01 20:19:44 +0530

    enhanced the textform component

--- my-app/src/components/TextForm.jsx
+++ my-app/src/components/TextForm.jsx
@@ -61,6 +61,22 @@ export default function TextForm(props) {
 
               setText(newext);
         }
+        const handleClearClick=()=>{
+          let newext='';
+          setText(newext);
+        }
+
+        const handleCopy=()=>{
+          let text =document.getElementById("myBox");
+          text.select();
+          navigator.clipboard.writeText(text.value);
+        }
+
+        const handleExtraSpaces=()=>{
+          let newext=text.split(/[ ]+/);
+          setText(newext.join(" "));
+        }
+
         const handleOnChange=(event)=>{
             console.log("on change");
             setText(event.target.value);
@@ -74,13 +90,16 @@ export default function TextForm(props) {
         
         <textarea style={{backgroundColor:props.mode==='light'?'white':'black',color:props.mode==='light'?'black':'white'}} className="form-control" id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
       </div>
-      <button className="btn btn-primary " onClick={handleUpClick} >Convert To Uppercase</button>
-      <button className="btn btn-primary mx-2" onClick={handleUpClick2} >Convert To Lowercase</button>
+      <button className="btn btn-primary m-2" onClick={handleUpClick} >Convert To Uppercase</button>
+      <button className="btn btn-primary m-2" onClick={handleUpClick2} >Convert To Lowercase</button>
+      <button className="btn btn-primary m-2" onClick={handleClearClick} >Clear Text</button>
+      <button className="btn btn-primary m-2" onClick={handleCopy} >Copy Text</button>
+      <button className="btn btn-primary m-2" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
     </div>
     <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
         <h1>Your Text Summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{0.008*(text.split(" ").length)} minutes it will take to read this..</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008*(text.split(" ").filter((element)=>{return element.length!==0}).length)} minutes it will take to read this..</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter text to Preview"}</p>
     </div>
